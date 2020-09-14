b5862a64a9f3697ade0235d1a1ed0914dc0f8385
RohitcoolKashyap <rohit.cse18@chitkarauniversity.edu.in>
2020-09-14 22:18:07 +0530

    created server

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  MONGODB_URL:
-    process.env.MONGODB_URL ||
-    "mongodb+srv://rohit:supercell@cluster0.pk7ng.mongodb.net/Test?retryWrites=true&w=majority",
-  JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
+  MONGODB_URL: process.env.MONGODB_URL || "",
+  JWT_SECRET: process.env.JWT_SECRET || "something",
 };
